f385b0933b7db9788ce4125e3b84db48a7b748d1
Macbook Air de IT <Admin@MacBook-Air-de-Macbook-2.local>
2018-02-12 11:05:00 +0100

    first commit

---
+++ src/app/app.firebaseconfig.ts
@@ -0,0 +1,8 @@
+export var config = {
+    apiKey: "AIzaSyB2kai59EVc5nuqMjZ-8RZ609zQ5lwrh9E",
+    authDomain: "book-9fd44.firebaseapp.com",
+    databaseURL: "https://book-9fd44.firebaseio.com",
+    projectId: "book-9fd44",
+    storageBucket: "book-9fd44.appspot.com",
+    messagingSenderId: "545893674895"
+  };
